f9d322ad03ef08185912844851327d97ee2ff82d
MichelHanzenScheeren <michelhanzenscheeren@gmail.com>
2020-05-10 19:46:45 -0300

    Criação da UI básica do app.

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:mobxgerencialistadetarefas/pages/login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MobX - Lista de Tarefas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.deepPurpleAccent,
+        cursorColor: Colors.deepPurpleAccent,
+        scaffoldBackgroundColor: Colors.deepPurpleAccent,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
